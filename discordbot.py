2a2ace695be373ad989e55484a61246316c39524
pientaki <86162193+pientaki@users.noreply.github.com>
2021-06-20 22:33:21 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -1,2 +1,7 @@
-import discord
+@bot.event
+async def on_command_error(ctx, error):
+    orig_error = getattr(error, "original", error)
+    error_msg = traceback.format_exception(type(orig_error), orig_error, orig_error.__traceback__)
+    error_msg = ''.join(traceback.TracebackException.from_exception(orig_error).format())
+    await ctx.send(error_msg)
 
